2e975e62d79d9582c07f02d453d98d9b59f4dbb0
anuja-amrale <anujaamrale1121@gmail.com>
2024-07-11 22:21:47 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,17 @@
+s="Hello Harry"
+x=10
+y=str(x)
+s1=y+s
+print(s1)
+print(s[-1])
+print(s[6:])
+print(s[-5:-2])
+print(s[4::])
+print(s.slice(0,10,1))
+s1=s.zfill(28)
+print(s1)
+       
+s1="#".join(s)
+
+
+print(s1)
